70dc0e46adb3b37079bc55ed0c4d6587c4a529ab
Oleg <olmag@inbox.ru>
2023-01-19 01:57:41 +0300

    task10, var 1

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 10: Напишите программу, которая принимает
+// на вход трёхзначное число и на выходе показывает вторую цифру
+// этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+// Решение 1
+
+begin:
+
+Console.Write("Введите трехзначное число: ");
+string text = Console.ReadLine();
+
+if (text.Length != 3)
+{
+    System.Console.WriteLine("неверный ввод");
+    goto begin;
+}
+else
+{
+    System.Console.WriteLine(text[1]);
+}
